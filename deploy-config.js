e8b338264bd5f192aa352d0bac993a8e5dfd8ced
Nicu Oprisan <nicu.oprisan@gmail.com>
2019-12-21 18:02:50 +0200

    Migrations and contract setup

---
+++ deploy-config.js
@@ -0,0 +1,57 @@
+const {ether} = require('@openzeppelin/test-helpers');
+const {BigNumber} = require('./test/helper');
+
+const stakingConfig = {
+    maxAmount: ether(BigNumber(5e+9)),
+    initialAmount: ether(BigNumber(500e+6)),
+    daysInterval: BigNumber(3),
+    unstakingPeriod: BigNumber(7),
+    maxIntervals: BigNumber(10),
+};
+
+const rewardsConfig = {
+    multiplier: BigNumber(5),
+    rewardRates: [
+        {
+            anualRewardRate: BigNumber(17),
+            lowerBound: BigNumber(0),
+            upperBound: ether(BigNumber(1.25e+9)),
+        },
+        {
+            anualRewardRate: BigNumber(19),
+            lowerBound: ether(BigNumber(1.25e+9)),
+            upperBound: ether(BigNumber(2.5e+9)),
+        },
+        {
+            anualRewardRate: BigNumber(21),
+            lowerBound: ether(BigNumber(2.5e+9)),
+            upperBound: ether(BigNumber(3.75e+9)),
+        },
+        {
+            anualRewardRate: BigNumber(23),
+            lowerBound: ether(BigNumber(3.75e+9)),
+            upperBound: ether(BigNumber(5e+9)),
+        },
+    ]
+};
+
+const anualRewardRates = rewardsConfig.rewardRates.map(rewardRate => rewardRate.anualRewardRate.toString());
+const lowerBounds = rewardsConfig.rewardRates.map(rewardRate => rewardRate.lowerBound.toString());
+const upperBounds = rewardsConfig.rewardRates.map(rewardRate => rewardRate.upperBound.toString());
+
+const ownerAddress = "0x585126227843F8C40A7047d008992E31de1dC7fa";
+const tokenAddress = "";
+const rewardsAddress = "";
+const rewardsAmount = ether(BigNumber(400e+6));
+
+module.exports = {
+    stakingConfig,
+    ownerAddress,
+    tokenAddress,
+    rewardsAddress,
+    rewardsAmount,
+    rewardsConfig,
+    anualRewardRates,
+    lowerBounds,
+    upperBounds
+};
---
+++ migrations/2_staking_contract_migration.js
@@ -0,0 +1,58 @@
+const {
+    stakingConfig,
+    ownerAddress,
+    anualRewardRates,
+    lowerBounds,
+    upperBounds,
+    rewardsAmount,
+    rewardsConfig,
+} = require('../deploy-config');
+
+let {rewardsAddress, tokenAddress} = require('../deploy-config');
+
+const StakingContract = artifacts.require("StakingContract");
+const Token = artifacts.require("Token");
+
+module.exports = async function (deployer, network, [owner]) {
+    let token;
+
+    if (network !== 'mainnet') {
+        rewardsAddress = owner;
+        await deployer.deploy(Token, 'Elrond Token', 'ERD', '18');
+        token = await Token.deployed();
+        tokenAddress = Token.address;
+
+        console.log(`Minting ${rewardsAmount.toString()} tokens for address: '${rewardsAddress}'`, '---------------------------');
+        await token.mint(rewardsAddress, rewardsAmount);
+    }
+
+    // Deploy the Staking Contract
+    await deployer.deploy(StakingContract, tokenAddress, rewardsAddress);
+    const stakingContract = await StakingContract.deployed();
+
+    if (network !== 'mainnet') {
+        console.log(`Approving ${rewardsAmount.toString()} tokens for contract: '${StakingContract.address}'`);
+        await token.approve(StakingContract.address, rewardsAmount);
+    }
+
+    // Setup the contract
+    console.log(`Setup staking limit 'StakingContract'`);
+    await stakingContract.setupStakingLimit(
+        stakingConfig.maxAmount, stakingConfig.initialAmount, stakingConfig.daysInterval, stakingConfig.unstakingPeriod
+    );
+
+    console.log(`Setup rewards 'StakingContract'`);
+    await stakingContract.setupRewards(
+        rewardsConfig.multiplier,
+        anualRewardRates,
+        lowerBounds,
+        upperBounds
+    );
+    console.log(`Unpausing 'StakingContract'`);
+    await stakingContract.unpause();
+
+    // Change owner
+    console.log(`Transferring ownership 'StakingContract'`);
+    console.log(`from: '${owner}'`, `to: ${ownerAddress}`);
+    await stakingContract.transferOwnership(ownerAddress);
+};
