01e5b8778ea43342d54e5826fcc2990841ee620f
Nicu Oprisan <nicu.oprisan@gmail.com>
2019-12-24 15:47:27 +0200

    currentStakingLimit() to external

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -218,7 +218,7 @@ contract StakingContract is Pausable, ReentrancyGuard {
 
     // VIEW FUNCTIONS FOR HELPING THE USER AND CLIENT INTERFACE
     function currentStakingLimit()
-    public
+    external
     onlyAfterSetup
     view
     returns (uint256)
